c2bc35fd72fabfe9718caffb876365b5249e5a56
Shruti Angane <shrutiangane628@gmail.com>
2023-03-31 09:33:00 +0530

    added project mocks under portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,8 +10,8 @@ const Portfolio = (props) => {
       </div>
       <div className='flex flex-col justify-evenly items-center md:grid md:grid-cols-2 gap-[30px] md:gap-[60px] mt-5'>
         {projects.map((element)=>{
-          return <div key={element.id} className={`flex flex-col items-center h-[320px] w-[300px] mt-5 rounded-lg hover:cursor-pointer hover:scale-105 duration-500 ${props.mode?'customshadow-dark':'customshadow-light border-2 border-teal-500'}`}>
-            <img src={element.img} alt='project'></img>
+          return <div key={element.id} className={`flex flex-col items-center h-[300px] w-[300px] mt-5 rounded-lg hover:cursor-pointer hover:scale-105 duration-500 ${props.mode?'customshadow-dark':'customshadow-light border-2 border-teal-500'}`}>
+            <img src={element.img} alt='project' className='rounded-t-lg'></img>
             <h4 className={`${props.mode?'text-[#FFFFFF]':'text-[#706C6C]'} text-[16px] font-medium m-[22px] font-bree`}>{element.title}</h4>
             <div className='flex justify-evenly items-center w-full border-t-2 h-full'>
               <div className='w-full flex justify-center border-r-2 h-full items-center'><a href={element.demo} target='_blank' rel="noreferrer" className={`${props.mode?'text-[#FFFFFF]':'text-[#706C6C]'} text-[16px] font-normal font-bree`}>Demo</a></div>
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,30 +1,33 @@
-import project from '../assets/project-sample.jpg'
+import pwd from '../assets/passgen.png'
+import text from '../assets/textcon.png'
+import news from '../assets/webtoday.png'
+import product from '../assets/phys.png'
 export const projects=[
     {
         id:1,
         title:'Password Generator',
-        img:project,
+        img:pwd,
         code:'https://github.com/ShrutiAngane/Password-Generator',
         demo:'https://shrutiangane.github.io/Password-Generator/'
     },
     {
         id:2,
         title:'Live News Website',
-        img:project,
+        img:news,
         code:'https://github.com/ShrutiAngane/News-WebApp',
         demo:'https://web-today-news.netlify.app/'
     },
     {
         id:3,
         title:'Text Convertor and translator',
-        img:project,
+        img:text,
         code:'https://github.com/ShrutiAngane/TextConverter',
         demo:'https://text-converter-app.netlify.app/'
     },
     {
         id:4,
         title:'Product landing page',
-        img:project,
+        img:product,
         code:'https://github.com/ShrutiAngane/Product-Landing-Page',
         demo:'https://product-landing-page-web.netlify.app/'
     }
